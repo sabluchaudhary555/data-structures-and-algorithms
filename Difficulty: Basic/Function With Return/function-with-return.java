3cec52d6c8883b829d32c7ebad246fd67f9683b2
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-02-09 22:31:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With Return/function-with-return.java
@@ -0,0 +1,7 @@
+class Solution {
+    // Function that takes an integer n and returns double its value
+    public int returnValueFunction(int n) {
+        // Use the return keyword to output twice the value of n
+        return n * 2;
+    }
+}
