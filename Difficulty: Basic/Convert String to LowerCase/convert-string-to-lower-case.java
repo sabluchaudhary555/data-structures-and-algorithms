3b70b9f9310bb38b2bad5ea111f1d931d6b46f60
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-02-09 21:46:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Convert String to LowerCase/convert-string-to-lower-case.java
@@ -0,0 +1,7 @@
+class Solution {
+    static String toLower(String s) {
+        // Use the built-in method to convert the entire string to lowercase
+        // This handles all uppercase characters (A-Z) in the string
+        return s.toLowerCase();
+    }
+}
