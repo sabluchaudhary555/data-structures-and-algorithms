792f2f813e60c85cddd89860094709b49abd9c64
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-02-10 21:14:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The New Line - Java/the-new-line-java.java
@@ -0,0 +1,12 @@
+class Geeks {
+    static void printIndividualLine() {
+        // Print the first word and move to a new line
+        System.out.println("Geeks");
+        
+        // Print the second word and move to a new line
+        System.out.println("for");
+        
+        // Print the third word and move to a new line
+        System.out.println("Geeks");
+    }
+}
