8b1ca120aac422830a6bc24cf2cb335f1cd2321c
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-02-14 22:50:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Swap The Numbers/swap-the-numbers.java
+++ Difficulty: Basic/Swap The Numbers/swap-the-numbers.java
@@ -1,21 +1,10 @@
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        // 1. Store value of 'a' in a temporary variable
+class Solution {
+    public void swap(int a, int b) {
+        // Using third variable method
         int temp = a;
-        
-        // 2. Assign value of 'b' to 'a'
         a = b;
-        
-        // 3. Assign value of 'temp' (original 'a') to 'b'
         b = temp;
-
+        
         System.out.println(a + " " + b);
     }
 }
