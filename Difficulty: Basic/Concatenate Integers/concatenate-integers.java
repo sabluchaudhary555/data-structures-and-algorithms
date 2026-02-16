827d1f7d641f25285d9b79280396a7bb7ed5ff45
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-02-16 22:46:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Concatenate Integers/concatenate-integers.java
@@ -0,0 +1,14 @@
+class GFG {
+    public static void main(String[] args) {
+        // The platform will provide input
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        utility(a, b);
+    }
+    
+    public static void utility(int a, int b) {
+        System.out.print(a);
+        System.out.print(b);
+    }
+}
