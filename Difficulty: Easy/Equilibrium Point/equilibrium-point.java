68def577857ce51698ba0b82c442b546a2f4fce2
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-03-06 21:41:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findEquilibrium(int[] arr) {
+        int totalSum = 0;
+        for (int num : arr) {
+            totalSum += num;
+        }
+        
+        int leftSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            totalSum -= arr[i];
+            
+            if (leftSum == totalSum) {
+                return i;
+            }
+            
+            leftSum += arr[i];
+        }
+        
+        return -1;
+    }
+}
