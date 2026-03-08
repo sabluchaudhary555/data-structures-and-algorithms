1d882495ea541efd16ca2ba41b7665d2d691313d
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-03-08 23:46:27 +0530

    Time: 5 ms (25.53%) | Memory: 43.9 MB (28.72%) - LeetSync

---
+++ 03-leetcode-solutions/43-multiply-strings/multiply-strings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+        
+        int m = num1.length(), n = num2.length();
+        int[] result = new int[m + n];
+        
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int product = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int sum = product + result[i + j + 1];
+                
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < result.length; i++) {
+            if (sb.length() == 0 && result[i] == 0) continue;
+            sb.append(result[i]);
+        }
+        
+        return sb.toString();
+    }
+}
