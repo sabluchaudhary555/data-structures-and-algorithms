560484675d6d98fd6911ec57a210d95f8ff218ff
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-03-23 20:19:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stickler Thief/stickler-thief.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMaxSum(int arr[]) {
+        int n = arr.length;
+        if (n == 1) return arr[0];
+        
+        int prev2 = arr[0];
+        int prev1 = Math.max(arr[0], arr[1]);
+        
+        for (int i = 2; i < n; i++) {
+            int current = Math.max(prev1, prev2 + arr[i]);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        
+        return prev1;
+    }
+}
