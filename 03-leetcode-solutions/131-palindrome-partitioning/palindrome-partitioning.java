5563ecf28d91666b5437d9333edc35fb35d3baa7
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-03-31 23:21:36 +0530

    Time: 8 ms (74.46%) | Memory: 65.3 MB (49.62%) - LeetSync

---
+++ 03-leetcode-solutions/131-palindrome-partitioning/palindrome-partitioning.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+    
+    private void backtrack(String s, int start, List<String> current, List<List<String>> result) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (isPalindrome(s, start, end - 1)) {
+                current.add(s.substring(start, end));
+                backtrack(s, end, current, result);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+    
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
