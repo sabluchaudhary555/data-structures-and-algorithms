6a2445230ed0b7479c004621f03a0eede24605ae
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-04-14 11:42:41 +0530

    Time: 2 ms (61.52%) | Memory: 43.6 MB (52.18%) - LeetSync

---
+++ 03-leetcode-solutions/140-word-break-ii/word-break-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        Map<Integer, List<String>> memo = new HashMap<>();
+        return dfs(s, 0, wordSet, memo);
+    }
+    
+    private List<String> dfs(String s, int start, Set<String> wordSet, Map<Integer, List<String>> memo) {
+        if (memo.containsKey(start)) {
+            return memo.get(start);
+        }
+        
+        List<String> result = new ArrayList<>();
+        
+        if (start == s.length()) {
+            result.add("");
+            return result;
+        }
+        
+        for (int end = start + 1; end <= s.length(); end++) {
+            String word = s.substring(start, end);
+            if (wordSet.contains(word)) {
+                List<String> suffixes = dfs(s, end, wordSet, memo);
+                for (String suffix : suffixes) {
+                    result.add(word + (suffix.isEmpty() ? "" : " " + suffix));
+                }
+            }
+        }
+        
+        memo.put(start, result);
+        return result;
+    }
+}
