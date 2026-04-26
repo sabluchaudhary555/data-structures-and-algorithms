e7491504f514e539318b318cc940f624198d20ad
Sablu Chaudhary <sabluchaudhary555@gmail.com>
2026-04-26 20:16:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Palindrome Array/palindrome-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static boolean isPalindrome(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while (left < right) {
+            if (arr[left] != arr[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
